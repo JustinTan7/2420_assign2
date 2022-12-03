dbfd6a82b0384afb6f239dfb94cc5d44a83fd0d9
Justin Tan <Justin>
2022-12-02 17:06:31 -0800

    changed this file to test it locally

--- src/index.js
+++ src/index.js
@@ -3,16 +3,20 @@ const fastify = require('fastify')({ logger: true })
 
 // Declare a route
 fastify.get('/', async (request, reply) => {
-	return { hello: '146.190.48.238' }
+	return { hello: 'Server x' }
 })
 
 //Run the server
 const start = async () => {
 	try {
-		await fastify.listen({ port: 3000 })
+		await fastify.listen({ port: 5050 })
 	} catch (err) {
 		fastify.log.error(err)
 		process.exit(1)
 	}
 }
 start()
+
+// /api
+// const stream = fs.createReadStream(("../html/index.html"))
+// await reply.send(stream)
